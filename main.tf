25e847192f79616de47be156aed72c875e57b7b2
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-05-03 15:24:05 +0000

    refactoring locals

--- main.tf
+++ main.tf
@@ -2,7 +2,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 locals {
-  name_suffix = "${var.resource_tags["project"]}-${var.resource_tags["environment"]}"
+  name_suffix = "${var.project_name}-${var.environment}"
+
+  required_tags = {
+    project     = var.project_name,
+    environment = var.environment
+  }
+  tags = merge(var.resource_tags, local.required_tags)
 }
 
 provider "aws" {
@@ -27,7 +33,7 @@ module "vpc" {
   enable_nat_gateway = true
   enable_vpn_gateway = var.enable_vpn_gateway
 
-  tags = var.resource_tags
+  tags = local.tags
 }
 
 module "app_security_group" {
@@ -40,7 +46,7 @@ module "app_security_group" {
 
   ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
 
-  tags = var.resource_tags
+  tags = local.tags
 }
 
 module "lb_security_group" {
@@ -53,7 +59,7 @@ module "lb_security_group" {
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  tags = var.resource_tags
+  tags = local.tags
 }
 
 resource "random_string" "lb_id" {
@@ -91,7 +97,7 @@ module "elb_http" {
     timeout             = 5
   }
 
-  tags = var.resource_tags
+  tags = local.tags
 }
 
 data "aws_ami" "amazon_linux" {
@@ -124,5 +130,5 @@ resource "aws_instance" "app" {
     echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
     EOF
 
-  tags = var.resource_tags
+  tags = local.tags
 }
